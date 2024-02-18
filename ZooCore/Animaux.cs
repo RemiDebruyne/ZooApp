6a3356e4487005933abf3792294309ca61121292
remiD <remidebruyne@hotmail.com>
2024-02-18 17:23:40 +0100

    Ajoutez des fichiers projet.

---
+++ ZooCore/Animaux.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZooCore
+{
+    internal abstract class Animaux
+    {
+        public int Id  { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public decimal? Weight { get; set; }
+        public decimal? Size { get; set; }
+        public string? Color { get; set; }
+        public Family Family { get; set; }
+        public Species Species { get; set; }
+    }
+
+    public enum Family
+    {
+        Felin,
+        Avian,
+        Ursidae,
+
+    } 
+
+    public enum Species
+    {
+        Tiger,
+        Lion,
+        BlackBear,
+        PolarBear,
+        Eagle,
+        Parrot,
+
+    }
+}
